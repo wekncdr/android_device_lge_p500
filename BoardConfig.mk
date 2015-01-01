82b7a8f2e2f02725412361878be745717d33f7a7
wekncdr <wekncdr@gmail.com>
2015-01-01 14:10:36 +0800

    Inherit vendor blobs and kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,12 +14,13 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p500/recovery/graphics.c
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-TARGET_KERNEL_SOURCE := kernel/lge/msm7x27-3.0.x
+TARGET_KERNEL_SOURCE := ../../../kernel/lge/msm7x27-3.0.x
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_p509_defconfig
 TARGET_SPECIFIC_HEADER_PATH := device/lge/p500/include
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 BOARD_KERNEL_BASE := 0x12800000
 BOARD_KERNEL_PAGESIZE := 2048
+-include ../../../kernel/lge/msm7x27-3.0.x/AndroidKernel.mk
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00440000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
--- full_p500.mk
+++ full_p500.mk
@@ -2,6 +2,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/p500/device.mk)
 $(call inherit-product-if-exists, vendor/lge/p500/p500-vendor.mk)
 
+$(call inherit-product, vendor/qcom/msm7x27/qcom-vendor.mk)
+$(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
+
 # See extras.txt (Gallery2, Provision, QuickSearchBox)
 $(call inherit-product-if-exists, vendor/google/gapps.mk)
 
